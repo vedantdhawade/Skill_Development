3081d120b515de8944836084216baf50484d397b
vedantdhawade <vedantdhawade1@gmail.com>
2025-04-18 22:16:10 +0530

    done

--- DSA/15. 3Sum.py
+++ DSA/15. 3Sum.py
@@ -1,8 +1,19 @@
 nums = [-1,0,1,2,-1,-4]
 nums.sort()
-print(len(nums))
+print(nums)
+arr = []
 for i in range(len(nums)-2):
     target = -nums[i]
+    print(target)
     left = i + 1
     right = len(nums) - 1
-    print(f"target - {target} left {left} right {right}")
+    while left < right :
+        if nums[left] + nums[right] == target:
+            arr.append([target , nums[left] , nums[right]])
+            print(arr)
+        elif nums[left] + nums[right] < target:
+            left = left + 1
+        else:
+            right = right + 1
+        left += 1
+        right -= 1
