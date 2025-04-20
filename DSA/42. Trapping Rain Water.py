4310ebfaea6f12062e93022aef934123425dd2da
vedantdhawade <vedantdhawade1@gmail.com>
2025-04-20 12:08:32 +0530

    Done

--- DSA/42. Trapping Rain Water.py
+++ DSA/42. Trapping Rain Water.py
@@ -1,23 +1,21 @@
-heights = [0,1,0,2,1,0,1,3,2,1,2,1]
-l = 0
-r = len(heights)-1
-water_trapped = 0
-while l < r :
-    max_l = heights[l]
-    max_r = heights[r]
-    if heights[l] < heights[r]:
-        if heights[l] > max_l:
-            max_l = heights[l]
-        else:
-            water_trapped = max_l - heights[l]
-        l = l + 1
-    else:
-        if heights[r] > max_r:
-            max_r = heights[r]
-        else:
-            water_trapped = max_r - heights[r]
-        r = r - 1
+def trap(height):
+    left, right = 0, len(height) - 1
+    left_max, right_max = 0, 0
+    water = 0
 
-    print(water_trapped)
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                water += left_max - height[left]
+            left += 1
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                water += right_max - height[right]
+            right -= 1
 
-        
+    return water
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
