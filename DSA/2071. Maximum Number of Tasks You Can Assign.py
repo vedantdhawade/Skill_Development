3bb1fdf2949a4db27a07345d92ee511ab9ad7df3
vedantdhawade <vedantdhawade1@gmail.com>
2025-05-01 21:05:34 +0530

    Done

--- DSA/2071. Maximum Number of Tasks You Can Assign.py
+++ DSA/2071. Maximum Number of Tasks You Can Assign.py
@@ -1,6 +1,28 @@
-tasks = [3,2,1]
-workers = [0,3,3]
-pills = 1
-strength = 1
-p1 , p2 = 0 , 0 
-while (p1 < len(tasks) or p2 < len(workers) ):
+tasks = [10,15,30]
+workers = [0,10,10,10,10]
+pills = 3
+strength = 10
+tasks.sort()
+workers.sort()
+p1 , p2 ,count = 0 , 0 , 0 
+
+while (p1 < len(tasks) and p2 < len(workers)):
+    if workers[p2] < tasks[p1]:
+        if pills != 0:
+            if workers[p2] + strength >= tasks[p1]:
+                pills -= 1
+                p1 += 1
+                p2 += 1
+                count += 1
+            else:
+                p1 += 1
+                p2 += 1
+        else:
+            p1 += 1
+            p2 += 1
+    else :
+        count += 1
+        p1 += 1
+        p2 += 1
+
+print(f"count ----> {count}")
